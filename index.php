3b3692627e5732ccc0ca1a80bc0a8b9a53757c44
kinori9 <50355001+kinori9@users.noreply.github.com>
2020-08-13 17:22:08 +0900

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>편의점 갤러리</title>
+</head>
+<link rel="stylesheet" href="index.css">
+<body>
+    <h1>편의점 갤러리</h1>
+    <div id="menu">
+        <ul>
+            <li><a class="menuLink" href="#">CU</a></li>
+            <li><a class="menuLink" href="#">GS25</a></li>
+            <li><a class="menuLink" href="#">seveneleven</a></li>
+        </ul>
+    </div>
+</body>
+</html>
